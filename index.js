944c5d5c7e58976f09d7dc0c89aa86f751623f42
Kevin Martensson <kevinmartensson@gmail.com>
2016-01-08 16:55:11 +0100

    ES2015ify code and set minimum Node.js 4

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 'use strict';
-var arrify = require('arrify');
-var taskkill = require('taskkill');
-var Promise = require('pinkie-promise');
-var execa = require('execa');
+const arrify = require('arrify');
+const taskkill = require('taskkill');
+const execa = require('execa');
 
 function win(input, opts) {
 	return taskkill(input, {
 		force: opts.force,
 		// don't kill ourselves
-		filter: 'PID ne ' + process.pid
+		filter: `PID ne ${process.pid}`
 	});
 }
 
 function def(input, opts) {
-	var cmd = typeof input === 'string' ? 'killall' : 'kill';
-	var args = [input];
+	const cmd = typeof input === 'string' ? 'killall' : 'kill';
+	const args = [input];
 
 	if (opts.force) {
 		args.unshift('-9');
@@ -23,23 +22,20 @@ function def(input, opts) {
 	return execa(cmd, args);
 }
 
-module.exports = function (input, opts) {
+module.exports = (input, opts) => {
 	opts = opts || {};
 
-	var fn = process.platform === 'win32' ? win : def;
-	var errors = [];
+	const fn = process.platform === 'win32' ? win : def;
+	const errors = [];
 
 	// don't kill ourselves
-	input = arrify(input).filter(function (el) {
-		return el !== process.pid;
-	});
+	input = arrify(input).filter(x => x !== process.pid);
 
-	return Promise.all(input.map(function (input) {
-		return fn(input, opts).catch(function (err) {
-			errors.push('Killing process ' + input + ' failed: ' +
-				err.message.replace(/.*\n/, '').replace(/kill: \d+: /, '').trim());
+	return Promise.all(input.map(input => {
+		return fn(input, opts).catch(err => {
+			errors.push(`Killing process ${input} failed: ${err.message.replace(/.*\n/, '').replace(/kill: \d+: /, '').trim()}`);
 		});
-	})).then(function () {
+	})).then(() => {
 		if (errors.length > 0) {
 			throw new Error(errors.join('\n'));
 		}
